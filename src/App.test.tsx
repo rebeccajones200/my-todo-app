f387b1017940316c47b872be59dd93137a873ef4
rebecca jones <rebecca.jones@wpengine.com>
2025-03-25 12:47:11 +0000

    added add tasks test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,20 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent} from '@testing-library/react';
+import "@testing-library/jest-dom";
+import userEvent from '@testing-library/user-event';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+describe("Task Manager App", () => {
+  test("adds a new task", async () => {
+    render(<App />);
+
+    const input = screen.getByPlaceholderText("Enter a new task");
+    const addButton =screen.getByText("Add Task");
+
+    await userEvent.type(input, "New task");
+    fireEvent.click(addButton);
+
+    expect(screen.getByText("New Task")).toBeInTheDocument();
+  })
+}); 
+
